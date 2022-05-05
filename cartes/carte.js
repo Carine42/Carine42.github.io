71b204ef4f92f06dbd7093560fcd9e8c380e7394
Carine <carine.chatain@orange.fr>
2022-05-05 13:30:08 +0200

    Modification des fonds de carte

--- cartes/carte.js
+++ cartes/carte.js
@@ -4,24 +4,27 @@
 
       const map = L.map("map", {
         zoomSnap : 0.25,
-        minZoom: 4
+        minZoom: 3
       }).setView([46.9, 1.9], 6.25);
 
 
       const basemapLayers = {
   
-        Navigation: L.esri.Vector.vectorBasemapLayer("ArcGIS:Navigation", { apiKey: apiKey }).addTo(map),
-        Topographic: L.esri.Vector.vectorBasemapLayer("ArcGIS:Topographic", { apiKey: apiKey }).addTo(map),
-        "Gris clair": L.esri.Vector.vectorBasemapLayer("ArcGIS:LightGray", { apiKey: apiKey }).addTo(map),  
-        "Relief": L.esri.Vector.vectorBasemapLayer("ArcGIS:StreetsRelief", { apiKey: apiKey }).addTo(map),  
-        Territoire: L.esri.Vector.vectorBasemapLayer("ArcGIS:ChartedTerritory", { apiKey: apiKey }).addTo(map),
-        Pinceau: L.esri.Vector.vectorBasemapLayer("ArcGIS:ColoredPencil", { apiKey: apiKey }).addTo(map),
-        Nuit: L.esri.Vector.vectorBasemapLayer("ArcGIS:Nova", { apiKey: apiKey }).addTo(map),
-        Siècle: L.esri.Vector.vectorBasemapLayer("ArcGIS:Midcentury", { apiKey: apiKey }).addTo(map),
-        OSM: L.esri.Vector.vectorBasemapLayer("OSM:Standard", { apiKey: apiKey }).addTo(map),
-        "OSM : Rues": L.esri.Vector.vectorBasemapLayer("OSM:Streets", { apiKey: apiKey }).addTo(map),
+        "ArcGIS:Navigation": L.esri.Vector.vectorBasemapLayer("ArcGIS:Navigation", { apiKey: apiKey }).addTo(map),
+        "ArcGIS:Terrain:Base":  L.esri.Vector.vectorBasemapLayer("ArcGIS:Terrain:Base", { apiKey: apiKey }).addTo(map),
+        "ArcGIS:Community":  L.esri.Vector.vectorBasemapLayer("ArcGIS:Community", { apiKey: apiKey }).addTo(map),
+        "ArcGIS:Topographic": L.esri.Vector.vectorBasemapLayer("ArcGIS:Topographic", { apiKey: apiKey }).addTo(map),
+        "ArcGIS:LightGray": L.esri.Vector.vectorBasemapLayer("ArcGIS:LightGray", { apiKey: apiKey }).addTo(map),  
+        "ArcGIS:StreetsRelief": L.esri.Vector.vectorBasemapLayer("ArcGIS:StreetsRelief", { apiKey: apiKey }).addTo(map),  
+        "ArcGIS:ChartedTerritory": L.esri.Vector.vectorBasemapLayer("ArcGIS:ChartedTerritory", { apiKey: apiKey }).addTo(map),
+        "ArcGIS:ColoredPencil": L.esri.Vector.vectorBasemapLayer("ArcGIS:ColoredPencil", { apiKey: apiKey }).addTo(map),
+        "ArcGIS:Nova": L.esri.Vector.vectorBasemapLayer("ArcGIS:Nova", { apiKey: apiKey }).addTo(map),
+        "ArcGIS:Midcentury": L.esri.Vector.vectorBasemapLayer("ArcGIS:Midcentury", { apiKey: apiKey }).addTo(map),
+        "OSM:Standard": L.esri.Vector.vectorBasemapLayer("OSM:Standard", { apiKey: apiKey }).addTo(map),
+        "OSM:Streets": L.esri.Vector.vectorBasemapLayer("OSM:Streets", { apiKey: apiKey }).addTo(map),
         "OSM": L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(map),
-        Satellite: L.esri.Vector.vectorBasemapLayer("ArcGIS:Imagery", { apiKey: apiKey }).addTo(map)
+         Satellite: L.esri.Vector.vectorBasemapLayer("ArcGIS:Imagery", { apiKey: apiKey }).addTo(map),
+        "ArcGIS:LightGray:Base":  L.esri.Vector.vectorBasemapLayer("ArcGIS:LightGray:Base", { apiKey: apiKey }).addTo(map)
       };
       
       L.control.layers(basemapLayers, null, { collapsed: false }).addTo(map); 
